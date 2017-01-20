d05d70278edbd35cdc40872f9e00abd23a6f5702
Mile <milewow1@gmail.com>
2017-01-20 17:22:47 +0100

    Add detailed time info

--- CardInfo.js
+++ CardInfo.js
@@ -1,14 +1,58 @@
 /**
  * Created by Mile on 19-Jan-17.
  */
-import * as Exponent from 'exponent';
 import React, {Component} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
 
-export class CardInfo extends Component {
+var moment = require('moment');
 
+class CardInfo extends Component {
     render() {
-        console.log('CardInfo');
-        console.log(this.props);
-        return (<Text>kek</Text>);
+        var crawlDate = this.props.date;
+        hours = moment().utcOffset(1).subtract(moment(crawlDate, 'DD.MM.YYYY HH:mm:ss')).hours();
+        return (
+            <View style={styles.card}>
+                <View style={[styles.skyblue, styles.cardRow]}>
+                    <Text style={styles.bold}>{this.props.title}</Text>
+                </View>
+                <View style={[styles.powderblue, styles.cardRow]}>
+                    <Text>{this.props.description}</Text>
+                </View>
+                <View style={[styles.steelblue, styles.cardRow]}>
+                    <Text style={styles.smallText}>објавено пред {hours} часа - {this.props.site}</Text>
+                </View>
+            </View>
+        );
     }
 }
+let styles = StyleSheet.create({
+    card: {
+        flex: 1,
+        flexDirection: 'column',
+        borderColor: '#000033',
+        borderWidth: 1,
+        marginBottom: 12
+    },
+    bold: {
+        fontWeight: 'bold',
+        fontSize: 17
+    },
+    skyblue: {
+        backgroundColor: 'skyblue'
+    },
+    powderblue: {
+        backgroundColor: 'powderblue'
+    },
+    smallText: {
+        fontSize: 10
+    },
+    steelblue: {
+        backgroundColor: 'steelblue'
+    },
+    cardRow: {
+        paddingLeft: 10,
+        paddingRight: 10
+    }
+});
+
+export default CardInfo;
--- listview-example.js
+++ listview-example.js
@@ -10,54 +10,34 @@ import CardInfo from './CardInfo';
 const TRAININGS_API = 'http://mladi.ams.mk/eduservice.svc/GetTrainings';
 class ListViewBasics extends Component {
     constructor(props) {
-        console.log('constr');
         super(props);
         this.ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
         this.state = {
             dataSource: this.ds
         };
-        console.log('constr finish');
     }
 
     componentWillMount() {
-        console.log('will mount');
         let thisClassScoped = this;
         this.fetchData(TRAININGS_API).then(function (data) {
-            console.log('fetch data');
             thisClassScoped.setState({
                 dataSource: thisClassScoped.ds.cloneWithRows(data)
             });
-        }).catch((err) => console.log('error=>' + err));
-    }
-
-    componentWillUpdate() {
-        console.log('update');
-        let thisClassScoped = this;
-        this.fetchData(TRAININGS_API).then(function (data) {
-            console.log('fetch data');
-            thisClassScoped.setState({
-                dataSource: thisClassScoped.ds.cloneWithRows(data)
-            });
-        }).catch((err) => console.log('error=>' + err));
+        }).catch((err) => console.error('error=>' + err));
     }
 
     fetchData(query) {
-        console.log('fetch');
         return fetch(query)
             .then((response) => response.json())
-            .then((data) => {
-                return data;
-            })
+            .then((data) => data)
             .catch(function (err) {
                 console.error('error => ' + err);
             });
     }
 
     render() {
-        console.log('render');
         return (
             <View style={styles.container}>
-                {/*<CardInfo property="test"/>*/}
                 <ListView
                     dataSource={this.state.dataSource}
                     renderRow={this._renderRow}
@@ -67,13 +47,7 @@ class ListViewBasics extends Component {
     }
 
     _renderRow(rowData) {
-        return (
-            <View style={styles.card}>
-                <View style={[styles.skyblue, styles.cardRow]}><Text>{rowData.Title}</Text></View>
-                <View style={[styles.powderblue, styles.cardRow]}><Text>{rowData.Description}</Text></View>
-                <View style={[styles.steelblue, styles.cardRow]}><Text>{rowData.Link}</Text></View>
-            </View>
-        );
+        return (<CardInfo title={rowData.Title} site={rowData.EduSiteName} date={rowData.CrawlDate} description={rowData.Description}/>);
     }
 }
 let styles = StyleSheet.create({
@@ -81,26 +55,6 @@ let styles = StyleSheet.create({
         flex: 1,
         paddingTop: 22,
         margin: 10
-    },
-    card: {
-        flex: 1,
-        flexDirection: 'column',
-        borderColor: '#000033',
-        borderWidth: 1,
-        marginBottom: 12
-    },
-    skyblue: {
-        backgroundColor: 'skyblue'
-    },
-    powderblue: {
-        backgroundColor: 'powderblue'
-    },
-    steelblue: {
-        backgroundColor: 'steelblue'
-    },
-    cardRow: {
-        paddingLeft: 10,
-        paddingRight: 10
     }
 });
 
